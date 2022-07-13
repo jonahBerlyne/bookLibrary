94ba51c5a15c98243e6f7d96d976df35c897ec33
jonahBerlyne <jonshaber@gmail.com>
2022-07-13 17:52:17 -0400

    added further stylings

--- script.js
+++ script.js
@@ -7,20 +7,25 @@ newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 const closeBtn = document.getElementsByTagName('span')[0];
 closeBtn.addEventListener('click', () => popUpForm.style.display = 'none');
 
+const books = document.querySelector('#books');
 const book_list = document.querySelector('#book-list');
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 
 class Book {
-  constructor(title, author, pages) {
+  constructor(date, id, title, author, pages, read) {
+    this.date = date;
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
   } 
 }
 
-let theIndex = null;
+let theDate = new Date().getTime();
+let theId = null;
 let found = false;
 let bookAdded = false;
 
@@ -35,10 +40,23 @@ function makeId() {
 }
 
 function addBook() {
+  theId = makeId();
+  let idIsUnique = false;
+  while (!idIsUnique) {
+    let sameId = myLibrary.find(el => el.key == theId);
+    if (sameId !== theId) {
+      idIsUnique = true;
+      break;
+    }
+    theId = makeId();
+  }
   const book = new Book (
+    theDate,
+    theId,
     document.getElementById('title').value,
     document.getElementById('author').value,
-    document.getElementById('pages').value
+    document.getElementById('pages').value,
+    "Not Read"
   );
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].title == book.title && myLibrary[i].author == book.author) {
@@ -52,34 +70,30 @@ function addBook() {
     return;
   }
   myLibrary.push(book);
-  theIndex = makeId();
-  let idIsUnique = false;
-  while (!idIsUnique) {
-    let sameId = myLibrary.find(el => el.key == theIndex);
-    if (sameId !== theIndex) {
-      idIsUnique = true;
-      break;
-    }
-    theIndex = makeId();
-  }
   bookAdded = true;
 }
   
-function showBook() {
+function showBooks() {
     book_list.innerHTML = '';
     myLibrary.map(item => {
       let card = document.createElement("div");
-      card.setAttribute("id", `${item.key}`);
-      card.className = "card";
+      card.setAttribute("id", `${item.id}`);
+      card.classList.add("card");
       
-      let theTitle = document.createElement("div");
-      theTitle.textContent += (`Title: ${item.title}`);
+      let theTitleDiv = document.createElement("div");
+      let theTitleText = document.createElement("h3");
+      theTitleText.textContent += (`Title: ${item.title}`);
+      theTitleDiv.appendChild(theTitleText);
       
-      let theAuthor = document.createElement("div");
-      theAuthor.textContent += (`Author: ${item.author}`);
+      let theAuthorDiv = document.createElement("div");
+      let theAuthorText = document.createElement("h3");
+      theAuthorText.textContent += (`Author: ${item.author}`);
+      theAuthorDiv.appendChild(theAuthorText);
       
-      let thePages = document.createElement("div");
-      thePages.textContent += (`Pages: ${item.pages}`);
+      let thePagesDiv = document.createElement("div");
+      let thePagesText = document.createElement("h3");
+      thePagesText.textContent += (`Pages: ${item.pages}`);
+      thePagesDiv.appendChild(thePagesText);
       
       const remove_btn = document.createElement("button");
       remove_btn.className = "button";
@@ -88,34 +102,55 @@ function showBook() {
       
       const read_btn = document.createElement("button");
       read_btn.className = "button";
-      read_btn.textContent = "Not Read";
-      read_btn.addEventListener('click', readIt);
+      read_btn.textContent = item.read;
+      read_btn.addEventListener('click', (e) => readIt(e, item.id));
+
+      let theButtonsDiv = document.createElement("div");
+      theButtonsDiv.classList.add("card-btns");
+      theButtonsDiv.appendChild(remove_btn);
+      theButtonsDiv.appendChild(read_btn);
       
-      card.appendChild(theTitle);
-      card.appendChild(theAuthor);
-      card.appendChild(thePages);
-      card.appendChild(remove_btn);
-      card.appendChild(read_btn);
+      card.appendChild(theTitleDiv);
+      card.appendChild(theAuthorDiv);
+      card.appendChild(thePagesDiv);
+      card.appendChild(theButtonsDiv);
 
       book_list.appendChild(card);
     });
+    books.classList.remove("books-hide");
 }
 
 function removeBook(e) {
-  e.target.parentElement.remove();
+  e.target.parentElement.parentElement.remove();
   myLibrary.splice(e.target, 1);
-  localStorage.removeItem(e.target.parentElement.id);
+  localStorage.removeItem(e.target.parentElement.parentElement.id);
+  if (myLibrary.length === 0) {
+    books.classList.add("books-hide");
+  }
 }
 
-const readIt = e => e.target.textContent == "Not Read" ? e.target.textContent = "Read" : e.target.textContent = "Not Read";
+function readIt (e, id) {
+  if (e.target.textContent == "Not Read") {
+    e.target.textContent = "Read";
+  } else {
+    e.target.textContent = "Not Read";
+  } 
+  let book = JSON.parse(localStorage.getItem(id));
+  book = {
+    ...book,
+    "read": book.read === "Not Read" ? "Read" : "Not Read"
+  };
+  localStorage.setItem(id, JSON.stringify(book));
+} 
 
 function submitBook() {
   if (titleInput.value.length !== 0 && authorInput.value.length !== 0 && pagesInput.value >= 1) {
     addBook();
     if (!bookAdded) return;
     setData(titleInput.value, authorInput.value, pagesInput.value);
-    showBook();
-    theIndex = null;
+    showBooks();
+    theDate = new Date().getTime();
+    theId = null;
     document.querySelector('form').reset();
     bookAdded = false;
   }
@@ -129,33 +164,33 @@ document.addEventListener('DOMContentLoaded', ()=> {
 });
 
 function setData(bookTitle, bookAuthor, bookPages) {
-  let book = localStorage.getItem(`${theIndex}`);
+  let book = localStorage.getItem(`${theId}`);
   book = book ? JSON.parse(book) : {};
-  const theKey = "key";
-  book[theKey] = theIndex;
-  const theTitle = "title";
-  book[theTitle] = bookTitle;
-  const theAuthor = "author";
-  book[theAuthor] = bookAuthor;
-  const thePages = "pages";
-  book[thePages] = bookPages;
-  localStorage.setItem(theIndex, JSON.stringify(book));
+  const theDateKey = "date";
+  book[theDateKey] = theDate;
+  const theIdKey = "id";
+  book[theIdKey] = theId;
+  const theTitleKey = "title";
+  book[theTitleKey] = bookTitle;
+  const theAuthorKey = "author";
+  book[theAuthorKey] = bookAuthor;
+  const thePagesKey = "pages";
+  book[thePagesKey] = bookPages;
+  const theReadKey = "read";
+  book[theReadKey] = "Not Read";
+  localStorage.setItem(theId, JSON.stringify(book));
 }
 
 function restore() {
   let keys = Object.keys(localStorage);
   for (let i = 0; i < keys.length; i++) {
-    myLibrary.push(JSON.parse(localStorage.getItem(keys[i])));
+    myLibrary.unshift(JSON.parse(localStorage.getItem(keys[i])));
   }
-  myLibrary.sort((a, b) => {
-    if (a.title < b.title) return -1; 
-    if (a.title > b.title) return 1;
-    if (a.title == b.title) {
-      if (a.author < b.author) return -1;
-      if (a.author > b.author) return 1;
-    }
-  });
-  showBook();
+  if (myLibrary.length === 0) {
+    return;
+  }
+  myLibrary.sort((a, b) => a.date - b.date);
+  showBooks();
 }
 
 restore();
